7888dfd74cdddd1299ce5d10a01bd72e00bb356f
pilgr1m <vanjavolokha@gmail.com>
2022-06-22 14:36:37 +0300

    Setting app (eslint, tsconfig, package)
    • added setting in project

---
+++ src/components/TestComponents.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+type Props = {
+
+}
+
+export const TestComponent: FC<Props> = () => {
+  const a = 1
+
+  return (
+    <> some text in test component... </>
+  )
+}

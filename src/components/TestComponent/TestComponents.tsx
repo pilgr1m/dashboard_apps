0ef9577aed347bc6946b97919b95d4de2efc695d
pilgr1m <vanjavolokha@gmail.com>
2022-06-24 13:56:47 +0300

    First layout setting and some fixes
    • App.tsx: fix not worked TooltipComponent form syncfusion. (removed StrictMode)
    Added some layout for menu and routes.

--- src/components/TestComponent/TestComponents.tsx
+++ src/components/TestComponent/TestComponents.tsx
@@ -8,6 +8,6 @@ export const TestComponent: FC<Props> = () => {
   const a = 1
 
   return (
-    <> some text in test component... </>
+    <> type &apos fcts &apos (my custom templates) - and create FC component </>
   )
 }

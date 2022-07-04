5e78bcdb7e9de92f2245594c26446d3608b1894d
pilgr1m <vanjavolokha@gmail.com>
2022-07-04 15:24:43 +0300

    T9_Homepage_create_component
    • App.tsx:
    Updated layout
    • Button.tsx, ECommerce.tsx:
    Created and setting component. Added layout and logic.

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from 'react'
 
 type Props = {
-
+  color:string
+  bgColor:string
+  text:string
+  borderRadius:string
+  size: string
 }
 
-export const Button: FC<Props> = () => {
+export const Button: FC<Props> = ({
+  color,
+  bgColor,
+  text,
+  borderRadius,
+  size,
+}) => {
   const a = 1
 
   return (
-    <> Button... </>
+    <button
+      type="button"
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+    >
+      {text}
+    </button>
   )
 }

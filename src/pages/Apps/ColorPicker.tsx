89b2063eea5f1ce8327d204062bb8067c6b8f1a4
pilgr1m <vanjavolokha@gmail.com>
2022-07-06 17:45:20 +0300

    T13_2_Mini_Apps_sections_Kanban
    • ColorPicker.tsx:
    Created component and setting it.

--- src/pages/Apps/ColorPicker.tsx
+++ src/pages/Apps/ColorPicker.tsx
@@ -1,13 +1,56 @@
 import React, { FC } from 'react'
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs'
+import { Header } from '../../components'
 
 type Props = {
 
+}
+
+const change = (args: Record<string, any>) => {
+  const previewElement = document.getElementById('preview')
+
+  if (previewElement) {
+    previewElement.style.backgroundColor = args.currentValue.hex
+  }
 }
 
 export const ColorPicker: FC<Props> = () => {
   const a = 1
 
   return (
-    <> ColorPicker... </>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Mini Apps" title="ColorPicker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">
+              Inline Pallete
+            </p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">
+              Inline Picker
+            </p>
+            <ColorPickerComponent
+              id="inline-picker"
+              mode="Picker"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }

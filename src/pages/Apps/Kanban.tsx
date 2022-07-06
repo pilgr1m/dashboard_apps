943e0b6a1b52fe78c4d90ba218e8628263301db6
pilgr1m <vanjavolokha@gmail.com>
2022-07-06 15:48:27 +0300

    T13_2_Mini_Apps_sections_Kanban
    • Kanban.tsx:
    Create new component and setting it.

--- src/pages/Apps/Kanban.tsx
+++ src/pages/Apps/Kanban.tsx
@@ -1,4 +1,8 @@
 import React, { FC } from 'react'
+import { KanbanComponent, ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-kanban'
+import { kanbanData, kanbanGrid } from '../../data/dummy'
+import { Header } from '../../components'
+import { uniqueId } from 'lodash'
 
 type Props = {
 
@@ -8,6 +12,20 @@ export const Kanban: FC<Props> = () => {
   const a = 1
 
   return (
-    <> Kanban... </>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Mini Apps" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+        keyField="Status"
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map(item => (
+            <ColumnDirective key={uniqueId('KeyKanban_')} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   )
 }

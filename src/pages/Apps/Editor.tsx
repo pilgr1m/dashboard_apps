1c2eecf16188c5f8abc16d97c8c518a4f0b701c5
pilgr1m <vanjavolokha@gmail.com>
2022-07-06 16:01:44 +0300

    T13_3_Mini_Apps_sections_Editor
    • dummy.tsx:
    Updated text for Editor component.
    • Editor.tsx:
    Create component and setting it.

--- src/pages/Apps/Editor.tsx
+++ src/pages/Apps/Editor.tsx
@@ -1,4 +1,16 @@
 import React, { FC } from 'react'
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from '@syncfusion/ej2-react-richtexteditor'
+
+import { EditorData } from '../../data/dummy'
+import { Header } from '../../components'
 
 type Props = {
 
@@ -8,6 +20,15 @@ export const Editor: FC<Props> = () => {
   const a = 1
 
   return (
-    <> Editor... </>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Mini Apps" title="Editor" />
+      <RichTextEditorComponent>
+
+        {/* text in view as component (html) */}
+        {/* <EditorData /> */}
+
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
   )
 }
